c675025bcbb403998796949cfa8f7535c89a81ee
Willy <willy810308@gmail.com>
2022-05-31 14:55:18 +0800

    add all

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PhotoEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlImgShow = new System.Windows.Forms.Panel();
+            this.pbImgShow = new System.Windows.Forms.PictureBox();
+            this.btnL90 = new System.Windows.Forms.Button();
+            this.btnR90 = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPass = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.ckbDrawTime = new System.Windows.Forms.CheckBox();
+            this.pnlImgShow.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImgShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlImgShow
+            // 
+            this.pnlImgShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlImgShow.Controls.Add(this.pbImgShow);
+            this.pnlImgShow.Location = new System.Drawing.Point(12, 64);
+            this.pnlImgShow.Name = "pnlImgShow";
+            this.pnlImgShow.Size = new System.Drawing.Size(902, 677);
+            this.pnlImgShow.TabIndex = 0;
+            // 
+            // pbImgShow
+            // 
+            this.pbImgShow.Location = new System.Drawing.Point(0, 0);
+            this.pbImgShow.Name = "pbImgShow";
+            this.pbImgShow.Size = new System.Drawing.Size(523, 425);
+            this.pbImgShow.TabIndex = 0;
+            this.pbImgShow.TabStop = false;
+            this.pbImgShow.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImgShow_Paint);
+            this.pbImgShow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbImgShow_MouseDown);
+            this.pbImgShow.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbImgShow_MouseMove);
+            this.pbImgShow.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbImgShow_MouseUp);
+            // 
+            // btnL90
+            // 
+            this.btnL90.Location = new System.Drawing.Point(94, 12);
+            this.btnL90.Name = "btnL90";
+            this.btnL90.Size = new System.Drawing.Size(75, 46);
+            this.btnL90.TabIndex = 1;
+            this.btnL90.Text = "左90 (Q)";
+            this.btnL90.UseVisualStyleBackColor = true;
+            this.btnL90.Click += new System.EventHandler(this.btnL90_Click);
+            // 
+            // btnR90
+            // 
+            this.btnR90.Location = new System.Drawing.Point(175, 12);
+            this.btnR90.Name = "btnR90";
+            this.btnR90.Size = new System.Drawing.Size(75, 46);
+            this.btnR90.TabIndex = 1;
+            this.btnR90.Text = "右90 (E)";
+            this.btnR90.UseVisualStyleBackColor = true;
+            this.btnR90.Click += new System.EventHandler(this.btnR90_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(256, 12);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(75, 46);
+            this.btnZoomIn.TabIndex = 1;
+            this.btnZoomIn.Text = "IN";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(337, 12);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(75, 46);
+            this.btnZoomOut.TabIndex = 1;
+            this.btnZoomOut.Text = "OUT";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(13, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 46);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnPass
+            // 
+            this.btnPass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPass.Location = new System.Drawing.Point(756, 12);
+            this.btnPass.Name = "btnPass";
+            this.btnPass.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnPass.Size = new System.Drawing.Size(75, 46);
+            this.btnPass.TabIndex = 1;
+            this.btnPass.Text = "Pass (A)";
+            this.btnPass.UseVisualStyleBackColor = true;
+            this.btnPass.Click += new System.EventHandler(this.btnPass_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(837, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 46);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save (S)";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ckbDrawTime
+            // 
+            this.ckbDrawTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ckbDrawTime.Appearance = System.Windows.Forms.Appearance.Button;
+            this.ckbDrawTime.Location = new System.Drawing.Point(675, 12);
+            this.ckbDrawTime.Name = "ckbDrawTime";
+            this.ckbDrawTime.Size = new System.Drawing.Size(75, 46);
+            this.ckbDrawTime.TabIndex = 2;
+            this.ckbDrawTime.Text = "時間印 (T)";
+            this.ckbDrawTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ckbDrawTime.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 751);
+            this.Controls.Add(this.ckbDrawTime);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPass);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnR90);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnL90);
+            this.Controls.Add(this.pnlImgShow);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.pnlImgShow.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImgShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlImgShow;
+        private System.Windows.Forms.PictureBox pbImgShow;
+        private System.Windows.Forms.Button btnL90;
+        private System.Windows.Forms.Button btnR90;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnPass;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.CheckBox ckbDrawTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoEditor
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> qImgPath = new Queue<string>();
+        Queue<Image> qImages = new Queue<Image>();
+        Queue<string> qImgSavePath = new Queue<string>();
+        Queue<Image> qImageSave = new Queue<Image>();
+        ManualResetEvent mreLoad = new ManualResetEvent(false);
+        ManualResetEvent mreShow = new ManualResetEvent(false);
+
+        Image imgShow;
+        int nRotMode = 0;
+        Rectangle rFocusRegion;
+        Rectangle rShowRegion;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool bFrtLoad = false;
+
+        private void IniObject()
+        {
+            qImgPath.Clear();
+            qImgSavePath.Clear();
+            while (qImages.Count>0)
+            {
+                var img = qImages.Dequeue();
+                img.Dispose();
+            }
+            while (qImageSave.Count > 0)
+            {
+                var img = qImages.Dequeue();
+                img.Dispose();
+            }
+            rFocusRegion = new Rectangle();
+            rShowRegion = new Rectangle();
+            mreLoad.Set();
+            mreShow.Set();
+        }
+
+        private void thdImgLoad_DoWork()
+        {
+            while (qImgPath.Count > 0)
+            {
+                if (qImages.Count > 3)
+                {
+                    mreLoad.Reset();
+                    mreLoad.WaitOne();
+                }
+                string path = qImgPath.Dequeue();
+                Bitmap bmp = new Bitmap(path);
+                qImages.Enqueue(bmp);
+                qImgSavePath.Enqueue(path);
+                if (bFrtLoad)
+                {
+                    mreShow.Set();
+                    bFrtLoad = false;
+                }
+            }
+        }
+
+        private void ShowImage()
+        {
+            pnlImgShow.Invoke(new Action(() =>
+            {
+                if (imgShow.Width >= imgShow.Height)
+                {
+                    pbImgShow.Width = 900;
+                    pbImgShow.Height = 675;
+
+                    pnlImgShow.Width = 902;
+                    pnlImgShow.Height = 677;
+
+                    this.Width = 940;
+                    this.Height = 790;
+                }
+                else
+                {
+                    pbImgShow.Width = 675;
+                    pbImgShow.Height = 900;
+
+                    pnlImgShow.Width = 677;
+                    pnlImgShow.Height = 902;
+
+                    this.Width = 715;
+                    this.Height = 1015;
+                }
+                //pbImgShow.Width = imgShow.Width;
+                //pbImgShow.Height = imgShow.Height;
+                pbImgShow.Invalidate();
+            })
+                        );
+        }
+        private void thdImgShow_DoWork()
+        {
+            while (true)
+            {
+                if (qImages.Count > 0)
+                {
+                    imgShow = qImages.Dequeue();
+                    ShowImage();
+
+                    mreLoad.Set();
+                }
+                mreShow.Reset();
+                mreShow.WaitOne();
+            }
+        }
+
+        private void SetRotateMode(int nMode)
+        {
+            if (nMode < 0)
+                nMode += 4;
+            nMode %= 4;
+            switch (nMode)
+            {
+                case 1:
+                    
+                    break;
+
+            }
+        }
+
+        int nCutMode = 0; // 0:切寬；1:切高
+        float fRate;
+        private void pbImgShow_Paint(object sender, PaintEventArgs e)
+        {
+            if (imgShow != null)
+            {
+                float fWRate = (float)imgShow.Width / pnlImgShow.Width;
+                float fHRate = (float)imgShow.Height / pnlImgShow.Height;
+                fRate = fWRate > fHRate ? fWRate : fHRate;
+
+
+                if (imgShow.Width < imgShow.Height)
+                {
+                    if (imgShow.Height / 3 > imgShow.Width / 2)
+                        nCutMode = 1;
+                    else
+                        nCutMode = 0;
+                }
+                else
+                {
+                    if (imgShow.Width / 3 > imgShow.Height / 2)
+                        nCutMode = 0;
+                    else
+                        nCutMode = 1;
+                }
+                if (nCutMode == 1)
+                {
+                    int nimgShowW = (int)(imgShow.Width / fRate);
+                    int nimgShowH = imgShow.Width >= imgShow.Height ? (int)(nimgShowW / 1.5) : (int)(nimgShowW * 1.5);
+                    rFocusRegion.Size = new Size(nimgShowW, nimgShowH);
+
+                }
+                else
+                {
+                    int nimgShowH = (int)(imgShow.Height / fRate);
+                    int nimgShowW = imgShow.Width < imgShow.Height ? (int)(nimgShowH / 1.5) : (int)(nimgShowH * 1.5);
+                    rFocusRegion.Size = new Size(nimgShowW, nimgShowH);
+                }
+
+                rShowRegion.Size = new Size((int)(imgShow.Width / fRate), (int)(imgShow.Height / fRate));
+
+                pbImgShow.Size = rShowRegion.Size;
+
+                e.Graphics.DrawImage(imgShow, rShowRegion);
+                Pen pen = new Pen(Brushes.Blue, 3);
+                e.Graphics.DrawRectangle(pen, rFocusRegion);
+            }
+        }
+
+        private void btnPass_Click(object sender, EventArgs e)
+        {
+            if (imgShow == null)
+                return;
+            Image imgPass = imgShow;
+            imgShow = null;
+            imgPass.Dispose();
+            imgPass = null;
+
+            mreShow.Set();
+
+            rFocusRegion = new Rectangle();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            open.Filter = "Image files (*.jpg)|*.jpg";
+            if (open.ShowDialog() != DialogResult.OK)
+                return;
+
+            IniObject();
+
+            this.Text = open.SafeFileNames.Length.ToString();
+            bFrtLoad = true;
+
+            foreach (var path in open.FileNames)
+            {
+                qImgPath.Enqueue(path);
+            }
+
+            Thread thdImgLoad = new Thread(new ThreadStart(thdImgLoad_DoWork));
+            thdImgLoad.IsBackground = true;
+            thdImgLoad.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thdImgShow = new Thread(new ThreadStart(thdImgShow_DoWork));
+            thdImgShow.IsBackground = true;
+            thdImgShow.Start();
+        }
+
+        bool bMouseDown = false;
+        Point plastClick;
+        Point pFocusRegionXY;
+        private void pbImgShow_MouseDown(object sender, MouseEventArgs e)
+        {
+            bMouseDown = rFocusRegion.Contains(e.Location) && e.Button == MouseButtons.Left;
+            if (bMouseDown)
+            {
+                plastClick = e.Location;
+                pFocusRegionXY = rFocusRegion.Location;
+            }
+        }
+
+        private void pbImgShow_MouseUp(object sender, MouseEventArgs e)
+        {
+            bMouseDown = false;
+        }
+
+        private void pbImgShow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (bMouseDown)
+            {
+                if (nCutMode == 0)
+                {
+                    rFocusRegion.X = pFocusRegionXY.X + e.X - plastClick.X;
+
+                    if (rFocusRegion.X < 0)
+                        rFocusRegion.X = 0;
+                    if (rFocusRegion.Right >= pbImgShow.Width)
+                        rFocusRegion.X = pbImgShow.Width - 1 - rFocusRegion.Width;
+                }
+                else
+                {
+                    rFocusRegion.Y = pFocusRegionXY.Y + e.Y - plastClick.Y;
+
+                    if (rFocusRegion.Y < 0)
+                        rFocusRegion.Y = 0;
+                    if (rFocusRegion.Bottom >= pbImgShow.Height)
+                        rFocusRegion.Y = pbImgShow.Height - 1 - rFocusRegion.Height;
+                }
+                pbImgShow.Invalidate();
+            }
+        }
+
+        //int nSaveCnt = 0;
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (imgShow == null)
+                return;
+
+            Image imgPass = imgShow;
+            imgShow = null;
+            string path = qImgSavePath.Dequeue();
+
+            FileInfo f = new FileInfo(path);
+            string sCreationTime = f.CreationTime.ToString();
+
+            string sNewName = "_" + System.IO.Path.GetFileName(path);
+            string sNewPath = System.IO.Path.GetDirectoryName(path) + @"\4X6\";
+            if (!System.IO.Directory.Exists(sNewPath))
+                System.IO.Directory.CreateDirectory(sNewPath);
+            RectangleF rSave = new RectangleF(
+                fRate * rFocusRegion.X,
+                fRate * rFocusRegion.Y,
+                fRate * rFocusRegion.Width,
+                fRate * rFocusRegion.Height);
+
+            //nSaveCnt++;
+            Task.Run(new Action(() =>
+            {
+                Bitmap bmpSave = new Bitmap((int)rSave.Width, (int)rSave.Height);
+                var g = Graphics.FromImage(bmpSave);
+                g.DrawImage(imgPass, new RectangleF(0,0, rSave.Width, rSave.Height), rSave, GraphicsUnit.Pixel);
+
+                if (ckbDrawTime.Checked) g.DrawString(sCreationTime, new Font("Ariel", 120, FontStyle.Regular, GraphicsUnit.Pixel), Brushes.Red, new Point(10, 10));
+                bmpSave.Save(sNewPath + sNewName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                g.Dispose();
+                bmpSave.Dispose();
+
+                imgPass.Dispose();
+                imgPass = null;
+            }));
+            
+
+            mreShow.Set();
+
+            rFocusRegion = new Rectangle();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.A:
+                    btnPass.PerformClick();
+                    break;
+                case Keys.S:
+                    btnSave.PerformClick();
+                    break;
+                case Keys.Q:
+                    btnL90.PerformClick();
+                    break;
+                case Keys.E:
+                    btnR90.PerformClick();
+                    break;
+            }
+        }
+
+        private void btnR90_Click(object sender, EventArgs e)
+        {
+            if (imgShow == null)
+                return;
+
+            rFocusRegion.X = 0;
+            rFocusRegion.Y = 0;
+            imgShow.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            ShowImage();
+        }
+
+        private void btnL90_Click(object sender, EventArgs e)
+        {
+            if (imgShow == null)
+                return;
+
+            rFocusRegion.X = 0;
+            rFocusRegion.Y = 0;
+            imgShow.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            ShowImage();
+        }
+    }
+}
